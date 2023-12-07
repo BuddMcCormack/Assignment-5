b3a3b43a2cf698f0be6644479721e2ffa6eae186
BuddMcCormack <150727458+BuddMcCormack@users.noreply.github.com>
2023-12-07 17:42:31 -0500

    Player paddle moves and is confined to the screen

--- Pong vs AI/Assets/Scripts/Balls.cs
+++ Pong vs AI/Assets/Scripts/Balls.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Balls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    protected Rigidbody2D ridigbody;
+    public float speed = 10.0f;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        ridigbody = GetComponent<Rigidbody2D>();
     }
 }
