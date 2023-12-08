7c6e69d696bd543f606b546a2c6d6f958a141e1e
BuddMcCormack <150727458+BuddMcCormack@users.noreply.github.com>
2023-12-07 21:58:42 -0500

    AI, should work but it doesn't

--- Pong vs AI/Assets/Scripts/AIBall.cs
+++ Pong vs AI/Assets/Scripts/AIBall.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class AIBall : MonoBehaviour
+public class AIBall : Balls
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody Paddle;
 
-    // Update is called once per frame
-    void Update()
+    public void Awake()
     {
-        
+        Paddle = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        if (this.Paddle.velocity.x > 0.0f)
+        {
+            if (this.Paddle.position.y > this.transform.position.y)
+            {
+                ridigbody.AddForce(Vector2.up * this.speed);
+            }
+            else if (this.Paddle.position.y < this.transform.position.y)
+            {
+                ridigbody.AddForce(Vector2.down * this.speed);
+            }
+            else
+            {
+                if (this.transform.position.y > 0.0f)
+                {
+                    ridigbody.AddForce(Vector2.down * this.speed);
+                }
+                else if (this.transform.position.y < 0.0f)
+                {
+                    ridigbody.AddForce(Vector2.up * this.speed);
+                }
+            }
+        }
     }
 }
